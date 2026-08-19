496dcfd366e817005d0795f0b1e41e5f0cd22a96
ledeyonateur-coder <ledeyonateur@gmail.com>
2026-08-19 23:54:53 +0200

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -16,7 +16,7 @@ export const config = {
     tagline: "Atelier de couture Privé à Villefranche-de-Rouergue.",
     social: [
       { label: "Instagram", href: "https://instagram.com/{{HANDLE}}" },
-      { label: "TikTok", href: "https://tiktok.com/@{{HANDLE}}" },
+      { label: "Mail ", href: Sun.Fabriks@gmail.com "},
     ],
   },
 };
